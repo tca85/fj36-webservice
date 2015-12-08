0859b4609187036617b1c2d8b63406a7db94c8e6
tca85 <thiago.alves.85@gmail.com>
2015-12-08 21:35:43 -0200

    modificando o nome dos argumentos e da resposta

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -1,10 +1,14 @@
 package br.com.caelum.estoque.ws;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 /**
@@ -19,6 +23,10 @@ import javax.jws.WebService;
 public class EstoqueWS {
 	private Map<String, ItemEstoque> repositorio = new HashMap<>();
 	
+	//---------------------------------------------------------------------------------------------
+	/**
+	 * 
+	 */
 	public EstoqueWS() {
 		repositorio.put("SOA", new ItemEstoque("SOA", 5));
 		repositorio.put("TDD", new ItemEstoque("TDD", 1));
@@ -28,9 +36,33 @@ public class EstoqueWS {
 		repositorio.put("ARQ", new ItemEstoque("ARQ", 2));
 	}
 	
-	@WebMethod
-	public ItemEstoque getQuantidade(String codigo){
-		System.out.println(repositorio.get(codigo));
-		return repositorio.get(codigo);
+	//---------------------------------------------------------------------------------------------
+
+	/**
+	 * SOA Falacies - o correto é receber uma lista e retornar uma lista
+	 * sempre limitada em uma quantidade específica
+	 * 
+	 * @param codigos
+	 * @return
+	 */
+	@WebMethod(operationName="ItensPeloCodigo")
+	@WebResult(name="ItemEstoque")
+	public List<ItemEstoque> getQuantidade(@WebParam(name="codigo") List<String> codigos){
+		 List<ItemEstoque> itens = new ArrayList<ItemEstoque>();
+		 
+		 if (codigos == null || codigos.isEmpty()) {
+			return itens;
+		}
+		 
+		 for (String codigo : codigos) {
+			if (repositorio.containsKey(codigo)) {
+				itens.add(repositorio.get(codigo));
+			}
+		}
+		 
+		 return itens;
 	}
+	//---------------------------------------------------------------------------------------------
+	
+	
 }
